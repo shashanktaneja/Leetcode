10c9f3c2e2b726631063bdcc9cd838f8d1820760
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-14 20:10:09 +0530

    Time: 205 ms (31.15%), Space: 40 MB (63.30%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    map<int,vector<pair<int,int>>> mp;
+    int ans;
+    
+    int networkDelayTime(vector<vector<int>>& v, int n, int k) {
+        mp.clear();
+        for(int i=0;i<v.size();i++){
+            mp[v[i][0]].push_back({v[i][1],v[i][2]});
+        }
+        
+        ans = INT_MIN;
+        queue<pair<int,int> > q;
+        q.push({k,0});
+        int dis[n+1];
+        bool vis[n+1];
+        memset(vis,false,sizeof(vis));
+        vis[k] = true;
+        
+        for(int i=1;i<=n;i++){
+            dis[i] = INT_MAX;
+        }
+        
+        dis[k] = 0;
+        
+        while(!q.empty()){
+            pair<int,int> cur = q.front();
+            q.pop();
+            for(auto nbr:mp[cur.first]){
+                if(cur.second+nbr.second < dis[nbr.first]){
+                    vis[nbr.first] = true;
+                    q.push({nbr.first,cur.second+nbr.second});
+                    dis[nbr.first] = cur.second+nbr.second;
+                }
+            }
+        }
+        
+        bool val = false;
+        for(int i=1;i<=n;i++){
+            if(vis[i]==false){
+                val = true;
+                break;
+            }
+            ans = max(ans,dis[i]);
+        }
+        
+        if(val){
+            return -1;
+        }
+        
+        // dfs(0,n,k);
+        
+        return ans;
+    }
+};
