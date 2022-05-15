8fa9ad289adbe5658f021994076c6899c8f03ac7
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-15 14:27:42 +0530

    Time: 106 ms (83.76%), Space: 59.7 MB (94.18%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int cnt;
+    int ans;
+    
+    void dfs1(int cur,TreeNode* root){
+        if(root->left==NULL and root->right==NULL){
+            if(cnt<cur){
+                cnt = cur;
+            }
+            return;
+        }
+        
+        if(root->left){
+            dfs1(cur+1,root->left);
+        }
+        
+        if(root->right){
+            dfs1(cur+1,root->right);
+        }
+    }
+    
+    void dfs2(int cur,TreeNode* root){
+        if(root->left==NULL and root->right==NULL){
+            if(cur==cnt){
+                ans+=(root->val);
+            }
+            return;
+        }
+        
+        if(root->left){
+            dfs2(cur+1,root->left);
+        }
+        
+        if(root->right){
+            dfs2(cur+1,root->right);
+        }
+    }
+    
+    int deepestLeavesSum(TreeNode* root) {
+        cnt = INT_MIN,ans = 0;
+        dfs1(0,root);
+        dfs2(0,root);
+        
+        return ans;
+    }
+};
