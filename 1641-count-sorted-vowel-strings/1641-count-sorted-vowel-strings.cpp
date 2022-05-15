c1628323427920bc319b033247ff763b1b40b32e
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-15 17:09:26 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.82%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
+    int dp[60][10];
+    
     int solve(int n,char cur){
         if(n==0){
             return 1;
+        }
+        
+        if(dp[n][(cur-'a')]!=-1){       
+            return dp[n][(cur-'a')];
         }
         
         int ans = 0;
@@ -11,11 +17,18 @@ public:
             ans += solve(n-1,ch);
         }
         
-        return ans;
+        return dp[n][(cur-'a')] = ans;
     }
     
     int countVowelStrings(int n) {
-        int ans = solve(n,'a');        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=5;j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        int ans = solve(n,'a');   
+        
         return ans;
     }
 };
