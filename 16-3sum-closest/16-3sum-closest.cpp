d04dce6de720198247cd0a2eb95be1a74be04f6c
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-21 04:30:14 +0530

    Time: 28 ms (37.36%), Space: 10.1 MB (56.29%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -7,26 +7,25 @@ public:
         
         sort(v.begin(),v.end());
         
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                int s = j+1,e = n-1;
-                int tp = v[i]+v[j];
-                while(s<=e){
-                    int mid = (s+e)/2;                    
-                    if(tp+v[mid]<t){
-                        s = mid+1;
-                    }
-                    else{
-                        e = mid-1;
-                    }
-                    
-                    if(cur>abs(tp+v[mid]-t)){
-                        cur = abs(tp+v[mid]-t);
-                        ans = tp+v[mid];
-                    }
+        for(int j=0;j<n-2;j++){
+            int s = j+1,e = n-1;
+            while(s<e){                
+                int tp = v[s]+v[e]+v[j];
+                int gp = abs(tp-t);
+                
+                if(gp<cur){
+                    cur = gp;
+                    ans = tp;
                 }
+                
+                if(tp<t){
+                    s++;
+                }
+                else{
+                    e--;
+                }                
             }
-        }
+        }        
         
         return ans;
     }
