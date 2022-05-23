8b600062b42a7c07173b698cb27e09d250172ed0
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-24 01:56:41 +0530

    Time: 355 ms (54.18%), Space: 36 MB (40.01%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    int ans;
-    int dp[605][105][105];
+    int dp[601][101][101];
     
     int solve(vector<pair<int,int>> &v,int idx,int m,int n){
         if(m<0 || n<0){
-            // ans = max(ans,cnt);
             return -1;
         }
         
@@ -17,21 +15,13 @@ public:
             return dp[idx][m][n];
         }
         
-        // cout<<idx<<endl;
-        
-        int z = v[idx].first;
-        int o = v[idx].second;
-        
-        int op1 = 1+solve(v,idx+1,m-z,n-o);
+        int op1 = 1+solve(v,idx+1,m-v[idx].first,n-v[idx].second);
         int op2 = solve(v,idx+1,m,n);  
-        
-        // cout<<idx<<" "<<op1<<" "<<op2<<endl;
         
         return dp[idx][m][n] = max(op1,op2);
     }
     
     int findMaxForm(vector<string>& v, int m, int n) {
-        // ans = INT_MIN;
         for(int i=0;i<v.size();i++){
             for(int j=0;j<=m;j++){
                 for(int k=0;k<=n;k++){
@@ -55,7 +45,7 @@ public:
             tp.push_back({z,o});
         }
         
-        ans = solve(tp,0,m,n);    
+        int ans = solve(tp,0,m,n);    
         
         return ans;
     }
