4cb946b2e2986261fd46361b411e660eeb64022a
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-26 21:21:50 +0530

    Time: 323 ms (36.93%), Space: 22.2 MB (37.30%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -7,7 +7,7 @@ public:
             s+=v[i];
         }
         
-        int dp[n+3][s+3];
+        int dp[n+1][s+1];
         
         for(int i=0;i<n;i++){
             for(int j=0;j<=sum;j++){
