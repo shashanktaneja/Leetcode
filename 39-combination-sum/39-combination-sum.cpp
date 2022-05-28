ef3750fc5f323ef6dd9e97541857503e9bfc9429
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-29 01:33:13 +0530

    Time: 3 ms (97.06%), Space: 10.9 MB (59.27%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -3,41 +3,25 @@ public:
     vector<vector<int>> ans;
     vector<int> tp;
     
-    bool val(vector<int> &tp){
-        for(int i=0;i<ans.size();i++){
-            if(tp==ans[i]){
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    void solve(int s,int n,vector<int> &v,int t){
+    void solve(int idx,int s,int n,vector<int> &v,int t){
         if(s>=t){
             if(s==t){
-                vector<int> tp2 = tp;
-                sort(tp2.begin(),tp2.end());
-                if(val(tp2)){
-                    ans.push_back(tp2);                    
-                }
+                ans.push_back(tp);    
             }
             return;
         }
         
-        for(int i=0;i<n;i++){
+        for(int i=idx;i<n;i++){
             tp.push_back(v[i]);
-            solve(s+v[i],n,v,t);
+            solve(i,s+v[i],n,v,t);
             tp.pop_back();
         }        
     }
     
     vector<vector<int>> combinationSum(vector<int>& v, int t) {
         ans.clear();
-        int n = v.size();
-        
-        solve(0,n,v,t);
-        
+        int n = v.size();        
+        solve(0,0,n,v,t);        
         return ans;
     }
 };
