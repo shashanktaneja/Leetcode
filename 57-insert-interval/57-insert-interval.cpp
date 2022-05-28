94994289fd18f9ba14f46f5512f2e55d8a4e6f08
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-28 16:44:34 +0530

    Time: 35 ms (18.41%), Space: 17.3 MB (23.54%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& v, vector<int>& t) {
+        int n = v.size();        
+        vector<vector<int>> ans;
+        
+        if(n==0){
+            ans.push_back(t);
+            return ans;
+        }
+        
+        if(t[0]>v[n-1][1]){
+            v.push_back(t);
+            return v;
+        }
+        
+        if(t[1]<v[0][0]){
+            v.insert(v.begin(),t);
+            return v;
+        }
+        
+        int i=1;
+        while(i<n){
+            if(t[0]>v[i-1][1] and t[1]<v[i][0]){
+                v.insert(v.begin()+i,t);
+                return v;
+            }
+            i++;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(t[0]>=v[i][0] and t[1]<=v[i][1]){
+                return v;
+            }
+        }
+        
+        i=0;
+        
+        while(i<n){
+            if(t[0]<=v[i][1]){
+                if(t[0]<v[i][0]){
+                    v[i][0] = t[0];
+                }
+                
+                if(t[1]>v[i][1]){
+                    v[i][1] = t[1];
+                }
+                
+                ans.push_back(v[i]);
+                i++;
+                while(i<n){
+                    if(ans[ans.size()-1][1]<v[i][0]){
+                        break;
+                    }
+                    
+                    if(ans[ans.size()-1][1]>=v[i][1]){
+                        i++;
+                    }
+                    else{
+                        ans[ans.size()-1][1] = v[i][1];
+                        i++;
+                        break;
+                    }
+                }
+                
+                while(i<n){
+                    ans.push_back(v[i]);
+                    i++;
+                }
+                break;
+            }
+            ans.push_back(v[i]);
+            i++;
+        }
+        
+        return ans;
+    }
+};
