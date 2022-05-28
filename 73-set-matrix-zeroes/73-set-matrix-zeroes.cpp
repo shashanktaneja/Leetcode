22ddf392c191ac2a5ffa0aef466ef04a9eb14625
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-28 22:41:06 +0530

    Time: 14 ms (83.93%), Space: 13.2 MB (54.51%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,30 +1,72 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& v) {
-        set<int> r,c;
         int m = v.size();
         int n = v[0].size();
         
+        bool c=false,r=false;
+        
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            if(v[i][0]==0){
+                c = true;
+                break;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(v[0][i]==0){
+                r = true;
+                break;
+            }
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(v[i][j]==0){
-                    r.insert(i);
-                    c.insert(j);
+//                     if(i==0 and j==0){
+                        
+//                     }
+//                     else if(i==0){
+                        
+//                     }
+//                     else if(j==0){
+                        
+//                     }
+//                     else{
+                        v[i][0] = 0;
+                        v[0][j] = 0;
+                    // }
                 }
             }
         }
         
-        for(auto cur:r){
-            for(int i=0;i<n;i++){
-                v[cur][i] = 0;
+        for(int i=1;i<m;i++){
+            if(v[i][0]==0){
+                cout<<i<<endl;
+                for(int j=1;j<n;j++){
+                    v[i][j] = 0;
+                }
             }
         }
         
-        for(auto cur:c){
+        for(int j=1;j<n;j++){
+            if(v[0][j]==0){
+                for(int i=1;i<m;i++){
+                    v[i][j] = 0;
+                }
+            }
+        }
+        
+        if(c){
             for(int i=0;i<m;i++){
-                v[i][cur] = 0;
+                v[i][0]=0;
             }
         }
         
+        if(r){
+            for(int i=0;i<n;i++){
+                v[0][i]=0;
+            }
+        }        
     }
 };
