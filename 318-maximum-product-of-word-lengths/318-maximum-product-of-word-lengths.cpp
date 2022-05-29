0c1c6cde69bf634f5c56b1d4081dc3c3350b5a61
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-29 14:27:49 +0530

    Time: 123 ms (37.92%), Space: 15.6 MB (89.46%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -4,25 +4,31 @@ public:
         int n = v.size();
         int ans = 0;
         
-        vector<vector<int>> tp;
+        vector<int> tp(n,0);
         
         for(int i=0;i<n;i++){
-            vector<int> f(26,0);
+            // vector<int> f(26,0);
             for(int j=0;j<v[i].size();j++){
-                f[v[i][j]-'a']++;
+                // f[]++;
+                int cur = v[i][j]-'a';
+                tp[i] |= (1<<cur);
             }
-            tp.push_back(f);
+            // tp.push_back(f);
         }
         
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 bool val = true;
+                int tp1 = tp[i];
+                int tp2 = tp[j];
                 
-                for(int k=0;k<26;k++){
-                    if(tp[i][k]>0 and tp[j][k]>0){
+                while(tp1>0){
+                    if((tp1&1) and (tp2&1)){
                         val = false;
                         break;
                     }
+                    tp1/=2;
+                    tp2/=2;
                 }
                 
                 if(val){
