3cf94e4c77f64ed0732d8e43c1122109f08b6c1f
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-30 12:07:19 +0530

    Time: 6 ms (79.46%), Space: 7.5 MB (42.33%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     vector<vector<string>> ans;
+    vector<string> v;
     
-    bool valid(vector<string> &v,int i,int j,int n){
+    bool valid(int i,int j,int n){
         int x=i-1,y=j-1;
         
         while(x>=0 and y>=0){
@@ -26,16 +27,16 @@ public:
         return true;
     }
     
-    void solve(int i,int mask,int n,vector<string> v){
+    void solve(int i,int mask,int n){
         if(i==n){
             ans.push_back(v);
             return;
         }
         
         for(int j=0;j<n;j++){
-            if(valid(v,i,j,n) and (mask&(1<<j))==0){
+            if(valid(i,j,n) and (mask&(1<<j))==0){
                 v[i][j] = 'Q';
-                solve(i+1,mask|(1<<j),n,v);
+                solve(i+1,mask|(1<<j),n);
                 v[i][j] = '.';
             }
         }                
@@ -43,15 +44,9 @@ public:
     
     vector<vector<string>> solveNQueens(int n) {
         ans.clear();
-        vector<string> v;
         string tp(n,'.');
-        
-        for(int i=0;i<n;i++){
-            v.push_back(tp);
-        }
-        
-        solve(0,0,n,v);
-        
+        v.resize(n, string(tp));        
+        solve(0,0,n);        
         return ans;
     }
 };
