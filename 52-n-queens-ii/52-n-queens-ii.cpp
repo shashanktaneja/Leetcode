8d7c75ca25216b2fd3982ad00de0fd6bea7131d1
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-30 12:09:47 +0530

    Time: 4 ms (78.29%), Space: 6 MB (79.99%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int ans;
+    vector<string> v;
+    
+    bool valid(int i,int j,int n){
+        int x=i-1,y=j-1;
+        
+        while(x>=0 and y>=0){
+            if(v[x][y]=='Q'){
+                return false;
+            }
+            x--;
+            y--;
+        }
+        
+        x=i-1,y=j+1;
+        
+        while(x>=0 and y<n){
+            if(v[x][y]=='Q'){
+                return false;
+            }
+            x--;
+            y++;
+        }
+        
+        return true;
+    }
+    
+    void solve(int i,int mask,int n){
+        if(i==n){
+            ans++;
+            return;
+        }
+        
+        for(int j=0;j<n;j++){
+            if(valid(i,j,n) and (mask&(1<<j))==0){
+                v[i][j] = 'Q';
+                solve(i+1,mask|(1<<j),n);
+                v[i][j] = '.';
+            }
+        }                
+    }
+    
+    int totalNQueens(int n) {
+        ans = 0;
+        string tp(n,'.');
+        v.resize(n, string(tp));        
+        solve(0,0,n);        
+        return ans;
+    }
+};
