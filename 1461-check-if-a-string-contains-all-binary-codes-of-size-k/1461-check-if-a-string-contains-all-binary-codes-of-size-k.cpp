79ee3c5183d8fc44ba3e2aad5945f829c5ce378c
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-05-31 12:10:47 +0530

    Time: 344 ms (70.12%), Space: 42.2 MB (67.61%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -7,16 +7,28 @@ public:
         }
         
         string tp = s.substr(0,k);
-        set<string> S;
-        S.insert(tp);
+        map<int,int> mp;
+        int hash = 0;
+        int pos = 1;
         
+        for(int i=tp.size()-1;i>=0;i--){
+            if(tp[i]=='1'){
+                hash += pos;
+            }
+            pos*=2;
+        }
+        
+        mp[hash]++;        
         int req = (1<<k);
         
-        for(int i=1;i<n-k+1;i++){
-            tp+=s[i+k-1];
-            tp.erase(tp.begin());
-            S.insert(tp);
-            if(S.size()==req){
+        for(int i=k;i<n;i++){
+            hash = (hash<<1);
+            hash = (hash&(req-1));
+            int cur = (s[i]-'0');
+            hash |= cur;
+            
+            mp[hash]++;
+            if(mp.size()==req){
                 return true;
             }
         }
