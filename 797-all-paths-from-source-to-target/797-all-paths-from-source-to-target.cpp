fa871609df407510953dad436cbaf42bbd4c3c08
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-06-03 02:09:18 +0530

    Time: 31 ms (37.44%), Space: 15.7 MB (28.64%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void solve(int cur,vector<int> v,int n,vector<vector<int>>& mp){
+        if(cur==n-1){
+            ans.push_back(v);
+            return;
+        }
+        
+        for(auto nbr:mp[cur]){
+            v.push_back(nbr);
+            solve(nbr,v,n,mp);
+            v.pop_back();
+        }
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& mp) {
+        ans.clear();
+        int n = mp.size();
+        vector<int> v;
+        v.push_back(0);
+        
+        solve(0,v,n,mp);
+        
+        return ans;
+    }
+};
