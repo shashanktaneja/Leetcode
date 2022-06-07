d61aaf0e0df8dbf0060a7e9d1db6f350d261a8e2
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-06-08 02:15:34 +0530

    Time: 183 ms (17.99%), Space: 41.1 MB (6.51%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.cpp
+++ 528-random-pick-with-weight/528-random-pick-with-weight.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     vector<int> v;
-    vector<pair<int,int>> tp;
-    int n;
     int k=0;
     int si;
     
     Solution(vector<int>& w) {
         v.clear();
-        tp.clear();
-        n = w.size();
+        priority_queue<pair<int,int>> tp;
+        int n = w.size();
         int sum = 0;
         for(int i=0;i<n;i++){
             sum += w[i];
-            tp.push_back({w[i],i});
+            tp.push({w[i],i});
         }
-        
-        sort(tp.begin(),tp.end(),greater<pair<int,int>>());
         k=0;
         
-        for(int i=0;i<n;i++){
-            int ti = ((tp[i].first)*100)/sum;
+        while(!tp.empty()){
+            pair<int,int> cur = tp.top();
+            tp.pop();
+            
+            int ti = ((cur.first)*100)/sum;
             for(int j=0;j<ti;j++){
-                v.push_back(tp[i].second);
+                v.push_back(cur.second);
             }
             
-            if(ti==0 and tp[i].first!=0){
-                v.push_back(tp[i].second);
+            if(ti==0 and cur.first!=0){
+                v.push_back(cur.second);
             }
         }      
         
