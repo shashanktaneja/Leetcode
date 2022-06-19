ecde4db0f974eb0efe9a2f593cfedfe1506c6bfc
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-06-20 01:47:15 +0530

    Time: 43 ms (96.68%), Space: 31.7 MB (68.47%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& v, string &t) {
+        int n = v.size();
+        int m = t.size();
+        vector<vector<string>> ans;
+        sort(v.begin(),v.end());
+        
+        int i=0;
+        for(int l=1;l<=m;l++){
+            string cur = t.substr(0,l);
+            vector<string> tp;
+            while(i<n){
+                if(v[i].substr(0,l)!=cur){
+                    i++;
+                }
+                else{
+                    break;
+                }
+            }
+            
+            if(i<n){
+                tp.push_back(v[i]);
+                if(i+1<n and v[i+1].substr(0,l)==cur){
+                    tp.push_back(v[i+1]);
+                    if(i+2<n and v[i+2].substr(0,l)==cur){
+                        tp.push_back(v[i+2]);
+                    }
+                }
+            }
+            
+            ans.push_back(tp);
+        }
+        
+        
+        return ans;
+    }
+};
