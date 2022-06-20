b64abf568a5beef408c4629b3ddf3836e5ef4a85
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-06-21 01:23:20 +0530

    Time: 2718 ms (5.00%), Space: 656.5 MB (5.01%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,3 +1,86 @@
+class node{
+  public:
+    char data;
+    bool terminal;
+    unordered_map<char,node*> children;
+    
+    node(char d){
+        data = d;
+        terminal = false;
+    }
+};
+
+bool val;
+
+class trie{
+  public:
+    node* root;
+    trie(){
+        root = new node('\0');
+    }
+    
+    void insert(string &s){
+        int n = s.size();
+        node* tp = root;
+        for(int i=0;i<n;i++){
+            char cur = s[i];
+            if(tp->children.count(cur)){
+                tp = tp->children[cur];
+            }
+            else{
+                node *n = new node(cur);
+                tp->children[cur] = n;
+                tp = n;
+            }
+        }
+        
+        tp->terminal = true;
+    }
+    
+    void dfs(node *tp, vector<string> &temp,string s){
+        if(temp.size()==3){
+            return;
+        }
+        
+        char cur = tp->data;
+        
+        if(tp->terminal){
+            s+=cur;
+            temp.push_back(s);
+            s.pop_back();
+        }
+        
+        for(char ch = 'a';ch<='z';ch++){
+            if(tp->children.count(ch)){
+                s+=cur;
+                dfs(tp->children[ch],temp,s);
+                s.pop_back();     
+            }
+        }           
+    }
+    
+    void find(vector<string> &temp, string &s){
+        int n = s.size();
+        node* tp = root;
+        string s1 = "";
+        
+        for(int i=0;i<n;i++){
+            char cur = s[i];
+            s1 += cur;
+            if(tp->children.count(cur)){
+                tp = tp->children[cur];
+            }
+            else{
+                val = false;
+                return;
+            }
+        }
+        
+        s1.pop_back();
+        dfs(tp,temp,s1);
+    }    
+};
+
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& v, string &t) {
@@ -5,36 +88,27 @@ public:
         int m = t.size();
         vector<vector<string>> ans;
         sort(v.begin(),v.end());
+        trie T;
         
         int i=0;
         string cur = "";
         
-        for(int l=1;l<=m;l++){
-            cur += t[l-1];
-            vector<string> tp;
-            while(i<n){
-                if(v[i].substr(0,l)!=cur){
-                    i++;
-                }
-                else{
-                    break;
-                }
-            }
-            
-            if(i<n){
-                tp.push_back(v[i]);
-                if(i+1<n and v[i+1].substr(0,l)==cur){
-                    tp.push_back(v[i+1]);
-                    if(i+2<n and v[i+2].substr(0,l)==cur){
-                        tp.push_back(v[i+2]);
-                    }
-                }
-            }
-            
-            ans.push_back(tp);
+        for(int i=0;i<n;i++){
+            T.insert(v[i]);
         }
         
-        
+        for(int l=1;l<=m;l++){
+            val = true;
+            cur += t[l-1];
+            vector<string> tp;
+            T.find(tp,cur);   
+            if(!val){
+                ans.push_back(tp);
+                continue;
+            }
+            ans.push_back(tp);
+        }
+                
         return ans;
     }
 };
