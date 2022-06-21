20ef9d0d634c4460ff023c54ca2878feed54fa57
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-06-22 02:01:53 +0530

    Time: 116 ms (90.75%), Space: 53.6 MB (77.33%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -3,58 +3,37 @@ public:
     int furthestBuilding(vector<int>& v, int &b, int &l) {
         int n = v.size();
         int ans = 0;
-        int s=0,e=n-1;
-        vector<int> tp;
-        for(int i=0;i<n-1;i++){
-            if(v[i]-v[i+1]<0){
-                tp.push_back((-v[i]+v[i+1]));
+        priority_queue<int,vector<int>,greater<int>> pq;
+        int i=1;
+        while(i<n and l>0){
+            if(v[i]-v[i-1]<=0){
+                i++;
+                continue;
+            }
+            pq.push((v[i]-v[i-1]));
+            i++;
+            l--;
+        }
+        
+        while(i<n){
+            if(v[i]-v[i-1]<=0){
+                i++;
+                continue;
+            }
+            
+            pq.push((v[i]-v[i-1]));
+            int mi = pq.top();
+            pq.pop();
+            
+            if(b>=mi){
+                b-=mi;
+                i++;
             }
             else{
-                tp.push_back(0);
+                return i-1;
             }
         }
         
-        while(s<=e){
-            int mid = (s+e)/2;
-            
-            int j=l,k=b;
-            vector<int> pq;
-            for(int i=0;i<mid;i++){
-                pq.push_back(tp[i]);
-            }
-            sort(pq.begin(),pq.end(),greater<int>());
-            int i=0;
-            
-            while(i<mid){
-                int cur = pq[i];
-                
-                if(cur==0){
-                    i++;
-                    continue;
-                }
-                
-                if(j>0){
-                    j--;
-                    i++;
-                }
-                else if(k>=cur){
-                    i++;
-                    k-=cur;
-                }
-                else{
-                    break;
-                }
-            }
-            
-            if(i>=mid){
-                ans = mid;
-                s = mid+1;
-            }
-            else{
-                e = mid-1;
-            }
-        }
-        
-        return ans;
+        return i-1;
     }
 };
