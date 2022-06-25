48e7a174cfacdd6f5052546f6c7d197bb2d883cd
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-06-25 18:29:34 +0530

    Time: 47 ms (32.21%), Space: 27.1 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -18,7 +18,10 @@ public:
     bool checkPossibility(vector<int>& v) {
         int n = v.size();
         int i=0;
-        int cnt = 0;
+        
+        if(val(v)){
+            return true;
+        }
         
         while(i<n-1){
             if(v[i]<=v[i+1]){
@@ -26,24 +29,21 @@ public:
                 continue;
             }
             
-                int tp = v[i];
-                v[i] = v[i+1];
+            int tp = v[i];
+            v[i] = v[i+1];
+            if(val(v)){
+                return true;
+            }
+            else{
+                v[i] = tp;
+                v[i+1] = v[i];
                 if(val(v)){
                     return true;
                 }
-                else{
-                    v[i] = tp;
-                    v[i+1] = v[i];
-                    break;
-                }
-            
-            cnt++;
-            i++;
+            }
+            break;
         }        
         
-        if(val(v)){
-            return true;
-        }
         
         return false;        
     }
