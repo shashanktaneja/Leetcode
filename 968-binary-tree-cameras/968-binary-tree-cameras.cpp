683e6980fe89550df5ef8ca1a53f85c732c581e1
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-06-27 02:18:05 +0530

    Time: 11 ms (84.70%), Space: 21.3 MB (51.34%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -11,22 +11,65 @@
  */
 class Solution {
 public:
-     int res = 0;
+    int ans;
+    TreeNode* head;
     
-    int minCameraCover(TreeNode* root) 
-    {
-        return (dfs(root) < 1 ? 1 : 0) + res;
+    void solve(TreeNode* root){
+        if(root->left==NULL and root->right==NULL){
+            return;
+        }
+        
+        if(root->left){
+            solve(root->left);
+        }
+        
+        if(root->right){
+            solve(root->right);
+        }
+        
+        bool val = false;
+        int cnt = 0;
+        
+        if(root->left and (root->left->val)==0){
+            val = true;
+        }
+        else if(root->left and (root->left->val)==1){
+            cnt++;            
+        }
+        
+        if(root->right and (root->right->val)==0){
+            val = true;
+        }
+        else if(root->right and (root->right->val)==1){
+            cnt++;            
+        }
+        
+        if((root==head)){
+            if((cnt==0) || val){
+                root->val = 1;
+                ans++;
+            }
+            return;
+        }
+        
+        if(val){
+            root->val = 1;
+            ans++;
+        }
+        else if(cnt>0){
+            root->val = 2;
+        }
     }
-
-    int dfs(TreeNode* root) 
-    {
-        if (!root) return 2;
-        int left = dfs(root->left), right = dfs(root->right);
-        if (left == 0 || right == 0) 
-        {
-            res++;
+    
+    int minCameraCover(TreeNode* root) {
+        if(root->left==NULL and root->right==NULL){
             return 1;
         }
-        return left == 1 || right == 1 ? 2 : 0;
+        
+        head = root;        
+        ans = 0;
+        solve(root);
+        
+        return ans;
     }
 };
