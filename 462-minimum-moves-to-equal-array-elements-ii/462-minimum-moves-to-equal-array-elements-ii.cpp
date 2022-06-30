7c53be291ba63cfabd6652c136ad0fd416ec2846
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-06-30 16:15:08 +0530

    Time: 24 ms (17.14%), Space: 10.9 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -6,12 +6,7 @@ public:
         int s=0,m=0;
         sort(v.begin(),v.end());
         
-        if(n%2!=0){
-            m = v[n/2];
-        }
-        else{
-            m = ((v[(n/2)-1]+v[n/2])/2);
-        }
+        m = v[n/2];
         
         for(int i=0;i<n;i++){
             ans += abs(m-v[i]);
