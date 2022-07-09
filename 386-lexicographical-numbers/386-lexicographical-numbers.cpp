64f5827198921f8c1c2da79622496542a40ddc68
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-07-10 03:08:31 +0530

    Time: 36 ms (34.17%), Space: 12.2 MB (33.41%) - LeetHub

---
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> ans;
+    
+    void solve(int cur,int n){
+        if(cur>n){
+            return;
+        }
+        
+        ans.push_back(cur);
+        for(int j=0;j<10;j++){
+            solve(10*cur+j,n);
+        }
+    }
+    
+    vector<int> lexicalOrder(int n) {
+        ans.clear();
+        for(int i=1;i<10;i++){
+            solve(i,n);
+        }
+        
+        return ans;
+    }
+};
