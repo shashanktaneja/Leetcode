eefd2685ec612ae5c8728fdaae62f441c32794ae
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-07-10 16:31:06 +0530

    Time: 1909 ms (5.04%), Space: 314.4 MB (5.01%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,96 @@
+class node{
+  public:
+    int data;
+    unordered_map<int,node*> children;
+    bool terminal;
+    
+    node(int d){
+        data = d;
+        terminal = false;
+    }
+};
+
+class trie{
+  public:
+    node *root;
+    trie(){
+        root = new node(2);
+    }
+    
+    void insert(int n){
+        node *head = root;
+        for(int i=31;i>=0;i--){
+            if(((n>>i)&1)==0){
+                if(head->children.count(0)){
+                    head = head->children[0];
+                }
+                else{
+                    node *n = new node(0);
+                    head->children[0] = n;
+                    head = n;
+                }
+            }
+            else{
+                if(head->children.count(1)){
+                    head = head->children[1];
+                }
+                else{
+                    node *n = new node(1);
+                    head->children[1] = n;
+                    head = n;
+                }
+            }
+        }
+        
+        head->terminal = true;
+    }
+    
+    int find(int n){
+        int ans = 0;
+        node *head = root;
+        
+        for(int i=31;i>=0;i--){
+            if(((n>>i)&1)==0){
+                if(head->children.count(1)){
+                    head = head->children[1];
+                    ans += pow(2,i);
+                }
+                else{
+                    head = head->children[0];
+                }
+            }
+            else{
+                if(head->children.count(0)){
+                    head = head->children[0];
+                    ans += pow(2,i);
+                }
+                else{
+                    head = head->children[1];
+                }
+            }
+        }
+        
+        return ans;
+    }    
+};
+
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& v) {
+        int n = v.size();
+        int ans = 0;
+        
+        trie t;
+        
+        for(int i=0;i<n;i++){
+            t.insert(v[i]);
+        }
+        
+        for(int i=0;i<n;i++){
+            int cur = t.find(v[i]);
+            ans = max(ans,cur);
+        }        
+        
+        return ans;
+    }
+};
