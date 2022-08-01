d7c244268aa8991624a3ad857b8953a0ef1c66dc
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-08-01 20:59:20 +0530

    Time: 27 ms (6.51%), Space: 9.9 MB (8.43%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& v, string s) {
+        vector<string> ans;
+        int n = v.size();
+        int si = s.size();
+        string tp = s;
+        
+        for(int i=0;i<n;i++){
+            map<char,char> mp;
+            for(char ch='a';ch<='z';ch++){
+                mp[ch] = '0';
+            }
+            
+            vector<int> vis(26,0);
+            bool val = true;
+            
+            for(int j=0;j<si;j++){
+                char cur = s[j];
+                char req = v[i][j];
+                
+                if((mp[cur]==req) || (mp[cur]=='0' and vis[(req-'a')]==0)){
+                    mp[cur] = req;
+                    vis[req-'a'] = 1;
+                }
+                else{
+                    val = false;
+                    break;
+                }
+            }
+                        
+            if(val){
+                ans.push_back(v[i]);
+            }
+            
+            s = tp;
+        }
+        
+        
+        return ans;
+    }
+};
