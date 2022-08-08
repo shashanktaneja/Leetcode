010a9289c3c9a142e89a4160526d7be59a2ee23f
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-08-08 18:41:12 +0530

    Time: 7 ms (50.13%), Space: 11.6 MB (25.76%) - LeetHub

---
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> solve(string s){
+        vector<int> ans;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='+' || s[i]=='-' || s[i]=='*'){
+                vector<int> left = solve(s.substr(0,i));
+                vector<int> right = solve(s.substr(i+1));
+                
+                for(int j=0;j<left.size();j++){
+                    for(int k=0;k<right.size();k++){
+                        if(s[i]=='+'){
+                            ans.push_back(left[j]+right[k]);
+                        }
+                        else if(s[i]=='-'){
+                            ans.push_back(left[j]-right[k]);
+                        }
+                        else{
+                            ans.push_back(left[j]*right[k]);
+                        }
+                    }
+                }
+            }            
+        }
+        
+        if(ans.empty()){
+            ans.push_back(stoi(s));
+        }
+        
+        return ans;        
+    }
+    
+    vector<int> diffWaysToCompute(string &s) {
+        vector<int> ans = solve(s);   
+        
+        return ans;        
+    }
+};
