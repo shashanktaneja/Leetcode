c6b1c3488d4a7b26b23afd347c325b3213450785
Shashank Taneja <60657409+shashanktaneja@users.noreply.github.com>
2022-11-16 02:05:48 +0530

    Time: 59 ms (42.19%), Space: 13.2 MB (80.68%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,115 @@
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& v) {
+        int m = v.size();
+        int n = v[0].size();
+        int dp[m][n];
+        
+        if(n==1){
+            return {-1};
+        }
+        
+        for(int j=n-1;j>=0;j--){
+            if(j==0){
+                if(v[m-1][j]==-1){
+                    dp[m-1][j]=-1;
+                }
+                else{
+                    if(v[m-1][j+1]==-1){
+                         dp[m-1][j]=-1;
+                    }
+                    else{
+                         dp[m-1][j]=1;
+                    }                       
+                }
+            }
+            else if(j!=n-1){
+                if(v[m-1][j]==1){
+                    if(v[m-1][j+1]==-1){
+                        dp[m-1][j]=-1;
+                    }
+                    else{
+                        dp[m-1][j] = j+1;
+                    }
+                }
+                else{
+                    if(v[m-1][j-1]==-1){
+                        dp[m-1][j]=j-1;
+                    }
+                    else{
+                        dp[m-1][j] = -1;
+                    }
+                }
+            }
+            else{
+                if(v[m-1][j]==1){
+                    dp[m-1][j]=-1;
+                }
+                else{
+                    if(v[m-1][j-1]==1){
+                         dp[m-1][j]=-1;
+                    }
+                    else{
+                         dp[m-1][j]=n-2;
+                    }                       
+                }
+            }
+        }        
+                
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(j==0){
+                    if(v[i][j]==-1){
+                        dp[i][j]=-1;
+                    }
+                    else{
+                        if(v[i][j+1]==-1){
+                             dp[i][j]=-1;
+                        }
+                        else{
+                             dp[i][j]=dp[i+1][j+1];
+                        }                       
+                    }
+                }
+                else if(j!=n-1){
+                    if(v[i][j]==1){
+                        if(v[i][j+1]==-1){
+                            dp[i][j]=-1;
+                        }
+                        else{
+                            dp[i][j] = dp[i+1][j+1];
+                        }
+                    }
+                    else{
+                        if(v[i][j-1]==-1){
+                            dp[i][j]=dp[i+1][j-1];
+                        }
+                        else{
+                            dp[i][j] = -1;
+                        }
+                    }
+                }
+                else{
+                    if(v[i][j]==1){
+                        dp[i][j]=-1;
+                    }
+                    else{
+                        if(v[i][j-1]==1){
+                             dp[i][j]=-1;
+                        }
+                        else{
+                             dp[i][j]=dp[i+1][j-1];
+                        }                       
+                    }
+                }               
+            }
+        }
+        
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            ans.push_back(dp[0][i]);
+        }
+        
+        return ans;
+    }
+};
